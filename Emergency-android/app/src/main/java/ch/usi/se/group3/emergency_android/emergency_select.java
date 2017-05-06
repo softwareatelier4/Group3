8374469bf4f52bcdc307b8ec56ca37353074534c
Alexander Camenzind <camenal@usi.ch>
2017-05-06 22:07:24 +0200

    updated gitignore file for the android app and made the app runnable

--- Emergency-android/app/src/main/java/ch/usi/se/group3/emergency_android/emergency_select.java
+++ Emergency-android/app/src/main/java/ch/usi/se/group3/emergency_android/emergency_select.java
@@ -10,17 +10,17 @@ public class emergency_select extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency_select);
-
+    System.out.println("xd");
     // Example of a call to a native method
     TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
+//    tv.setText(stringFromJNI());
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+//    public native String stringFromJNI();
 
     // Used to load the 'native-lib' library on application startup.
     static {
