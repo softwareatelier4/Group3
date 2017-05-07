9a6c294b27c13c12f5f90f0f25ae4e110a828802
sreding <redins@usi.ch>
2017-05-07 18:03:46 +0200

    some changes to java

--- Emergency-android/app/src/main/java/ch/usi/se/group3/emergency_android/emergency_select.java
+++ Emergency-android/app/src/main/java/ch/usi/se/group3/emergency_android/emergency_select.java
@@ -13,14 +13,14 @@ public class emergency_select extends AppCompatActivity {
 
     // Example of a call to a native method
     TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
+//    tv.setText(stringFromJNI());
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+//    public native String stringFromJNI();
 
     // Used to load the 'native-lib' library on application startup.
     static {
