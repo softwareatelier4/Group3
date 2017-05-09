6e984eed2b75e498f2cfae9bf8d2ca71ea6147d5
Alexander Camenzind <camenal@usi.ch>
2017-05-09 14:05:14 +0200

    jenkins xvfb test

--- test.sh
+++ test.sh
@@ -2,8 +2,9 @@ npm install
 bower install
 node seed.js
 npm run test
-# npm start &
-# PID=$!
-# sleep 2
+npm start &
+PID=$!
+sleep 2
+xvfb-run -a ./nightwatch
 # ./nightwatch #the tests work if run on a local machine
-# kill $PID
+kill $PID
