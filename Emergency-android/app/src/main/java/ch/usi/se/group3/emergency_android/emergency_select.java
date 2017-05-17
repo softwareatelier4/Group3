516d5ceb9150f2a52df717601147ebd32c8eccf6
falcip <paolo.falcioni@usi.ch>
2017-05-17 14:43:09 +0200

    function for sign-in button on the app

--- Emergency-android/app/src/main/java/ch/usi/se/group3/emergency_android/emergency_select.java
+++ Emergency-android/app/src/main/java/ch/usi/se/group3/emergency_android/emergency_select.java
@@ -1,10 +1,24 @@
 package ch.usi.se.group3.emergency_android;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class emergency_select extends AppCompatActivity {
+    public void exampleFunction(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "PORCODIO!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+        // Kabloey
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
