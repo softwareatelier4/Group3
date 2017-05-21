a82ce14a413d4e15a57909aa564bd05d1eb6e790
Gianmarco <palazg@usi.ch>
2017-05-22 00:39:46 +0200

    tests

--- test/nightwatchTests/signUp.js
+++ test/nightwatchTests/signUp.js
@@ -16,3 +16,32 @@
 //       .end()
 //   }
 // }
+
+// module.exports = {
+//   'test':function(client){
+//     client
+//       .url("localhost:4000/index.html")// .url("localhost:4000/index.html")
+//       .useCss()
+//       .waitForElementVisible('body',1000)
+//       .assert.visible('#logo')
+//       .end()
+//   },
+//   'login-test+log -out':function(client){
+//     client
+//       .url("localhost:4000/login")
+//       .useCss()
+//       .waitForElementVisible('body',1000)
+//       .assert.visible("input[name=password]")
+//       .assert.visible("input[name=userName]")
+//       .setValue("input[name=password]","ciao")
+//       .setValue("input[name=userName]","masiar")
+//       .click("#login-button")
+//       .waitForElementVisible("big-component",1000)
+//       .pause(10000)
+//       .assert.visible("#logout-button")
+//       .click("#logout-button")
+//       .waitForElementVisible("#login-button",1000)
+//
+//       .end()
+//   }
+// }
