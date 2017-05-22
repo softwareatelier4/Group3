0556319c870ffd3c272846f226ad0ecad54ac970
Alexander Camenzind <camenal@usi.ch>
2017-05-22 02:20:05 +0200

    trying to resolve a conflict

--- Emergency-android/app/src/main/java/ch/usi/se/group3/emergency_android/FreelancerListViewAdapter.java
+++ Emergency-android/app/src/main/java/ch/usi/se/group3/emergency_android/FreelancerListViewAdapter.java
@@ -1,6 +1,7 @@
 package ch.usi.se.group3.emergency_android;
 
 import android.content.Context;
+import android.os.Handler;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.util.Log;
@@ -11,6 +12,13 @@ import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.ToggleButton;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -42,7 +50,7 @@ public class FreelancerListViewAdapter extends ArrayAdapter<Integer> {
             TextView textView2 = (TextView) v.findViewById(R.id.secondLine);
             textView2.setText(o.getString("firstName"));
             ToggleButton toggle = (ToggleButton) v.findViewById(R.id.toggleButton);
-            toggle.setOnCheckedChangeListener(new ClickHandler());
+            toggle.setOnCheckedChangeListener(new ClickHandler(o.getString("_id"),this.fl));
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -52,17 +60,50 @@ public class FreelancerListViewAdapter extends ArrayAdapter<Integer> {
 }
 
 class ClickHandler implements CompoundButton.OnCheckedChangeListener{
-    public ClickHandler(String id){
-
+    String id;
+    freelancer_list fl;
+    Handler h;
+    public ClickHandler(String id, freelancer_list fl){
+        this.id = id;
+        this.fl = fl;
+        h = new Handler();
     }
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        RequestQueue queue = Volley.newRequestQueue(fl);
+
+        //send available signal
+        String url="http://" + FreelancerDataHolder.getIp() + ":4000/freelancer/setAvailable/"+this.id;
         if (isChecked) {
-            Log.v("checked","checked");
-        } else {
+            Log.v("checked","checked"+this.id);
+            url+="?available=true";
+
+        } else {//send not available signal
             // The toggle is disabled
             Log.v("checked","not checked");
+            url+="?available=false";
         }
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.v("success","success");
+                        h.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                Log.v("d","asfd");
+                                fl.sendNotification("hribpiubuasdf","aslkfjdn");
+                            }
+                        },10000);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.v("no success","no success");
+            }
+        });
+        queue.add(stringRequest);
     }
 }
